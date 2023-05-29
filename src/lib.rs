c16009f551505b3d721e7e158948c4f5e9e1e0a6
Yuheng Su <gipsyh.icu@gmail.com>
2023-05-29 04:40:10 +0000

    update

--- src/lib.rs
+++ src/lib.rs
@@ -6,41 +6,57 @@ use std::{
     ops::{BitAnd, BitAndAssign, BitOr, BitOrAssign, BitXor, BitXorAssign, Not},
 };
 
-pub trait Bdd: Sized + PartialEq
+pub trait Bdd:
+    Sized
+    + PartialEq
+    + Eq
+    + Clone
+    + Debug
+    + 'static
+    + Not<Output = Self>
+    + BitAnd<Self, Output = Self>
+    + BitAndAssign<Self>
+    + BitOr<Self, Output = Self>
+    + BitOrAssign<Self>
+    + BitXor<Self, Output = Self>
+    + BitXorAssign<Self>
 where
-    Self: Not<Output = Self>
-        + BitAnd<Self, Output = Self>
-        + BitAndAssign<Self>
-        + BitOr<Self, Output = Self>
-        + BitOrAssign<Self>
-        + BitXor<Self, Output = Self>
-        + BitXorAssign<Self>,
     for<'a> Self: BitAnd<&'a Self, Output = Self>
         + BitAndAssign<&'a Self>
         + BitOr<&'a Self, Output = Self>
         + BitOrAssign<&'a Self>
         + BitXor<&'a Self, Output = Self>
         + BitXorAssign<&'a Self>,
-    for<'a, 'b> &'a Self: Not<Output = Self>
+    for<'a> &'a Self: Not<Output = Self>
         + BitAnd<Self, Output = Self>
-        + BitAnd<&'b Self, Output = Self>
         + BitOr<Self, Output = Self>
+        + BitXor<Self, Output = Self>,
+    for<'a, 'b> &'a Self: BitAnd<&'b Self, Output = Self>
         + BitOr<&'b Self, Output = Self>
-        + BitXor<Self, Output = Self>
         + BitXor<&'b Self, Output = Self>,
 {
+    fn size(&self) -> usize;
+
     fn is_constant(&self, val: bool) -> bool;
 
     fn if_then_else(&self, _then: &Self, _else: &Self) -> Self;
+
+    fn and_abstract<I: IntoIterator<Item = usize>>(&self, f: &Self, vars: I) -> Self;
+
+    fn previous_state(&self) -> Self;
 
     fn next_state(&self) -> Self;
 
     fn pre_image(&self, trans: &Self) -> Self;
 
     fn post_image(&self, trans: &Self) -> Self;
+
+    fn support(&self) -> Self;
+
+    fn support_index(&self) -> Vec<usize>;
 }
 
-pub trait BddManager: Sized + Clone + Debug
+pub trait BddManager: Sized + Clone + Debug + 'static
 where
     for<'a, 'b> &'a Self::Bdd: Not<Output = Self::Bdd>
         + BitAnd<Self::Bdd, Output = Self::Bdd>
@@ -59,4 +75,16 @@ where
     fn constant(&self, val: bool) -> Self::Bdd;
 
     fn ith_var(&self, var: usize) -> Self::Bdd;
+
+    fn num_var(&self) -> usize;
+
+    fn state_vars(&self) -> Vec<usize> {
+        (0..self.num_var()).filter(|x| x % 2 == 0).collect()
+    }
+
+    fn next_state_vars(&self) -> Vec<usize> {
+        (0..self.num_var()).filter(|x| x % 2 == 1).collect()
+    }
+
+    fn translocate(&self, bdd: &Self::Bdd) -> Self::Bdd;
 }
--- src/peabody.rs
+++ src/peabody.rs
@@ -1,44 +1,60 @@
-use crate::{Bdd, BddManager};
-use peabody::Peabody;
+// use crate::{Bdd, BddManager};
+// use peabody::Peabody;
 
-impl Bdd for peabody::Bdd {
-    fn is_constant(&self, val: bool) -> bool {
-        self.is_constant(val)
-    }
+// impl Bdd for peabody::Bdd {
+//     fn is_constant(&self, val: bool) -> bool {
+//         self.is_constant(val)
+//     }
 
-    fn if_then_else(&self, _then: &Self, _else: &Self) -> Self {
-        (self & _then) | (!self & _else)
-    }
+//     fn if_then_else(&self, _then: &Self, _else: &Self) -> Self {
+//         (self & _then) | (!self & _else)
+//     }
 
-    fn next_state(&self) -> Self {
-        self.next_state()
-    }
+//     fn and_abstract<I: IntoIterator<Item = usize>>(&self, _f: &Self, _vars: I) -> Self {
+//         todo!()
+//     }
 
-    fn pre_image(&self, trans: &Self) -> Self {
-        self.pre_image(trans)
-    }
+//     fn previous_state(&self) -> Self {
+//         self.original_state()
+//     }
 
-    fn post_image(&self, trans: &Self) -> Self {
-        self.post_image(trans)
-    }
-}
+//     fn next_state(&self) -> Self {
+//         self.next_state()
+//     }
 
-impl BddManager for Peabody {
-    type Bdd = peabody::Bdd;
+//     fn pre_image(&self, trans: &Self) -> Self {
+//         self.pre_image(trans)
+//     }
 
-    fn new() -> Self {
-        Peabody::new()
-    }
+//     fn post_image(&self, trans: &Self) -> Self {
+//         self.post_image(trans)
+//     }
+// }
 
-    fn new_with_capacity(capacity: usize) -> Self {
-        todo!()
-    }
+// impl BddManager for Peabody {
+//     type Bdd = peabody::Bdd;
 
-    fn constant(&self, val: bool) -> Self::Bdd {
-        self.constant(val)
-    }
+//     fn new() -> Self {
+//         Peabody::new()
+//     }
 
-    fn ith_var(&self, var: usize) -> Self::Bdd {
-        self.ith_var(var)
-    }
-}
+//     fn new_with_capacity(capacity: usize) -> Self {
+//         Peabody::new_with_capacity(capacity)
+//     }
+
+//     fn constant(&self, val: bool) -> Self::Bdd {
+//         self.constant(val)
+//     }
+
+//     fn ith_var(&self, var: usize) -> Self::Bdd {
+//         self.ith_var(var)
+//     }
+
+//     fn num_var(&self) -> usize {
+//         todo!()
+//     }
+
+//     fn translocate(&self, _bdd: &Self::Bdd) -> Self::Bdd {
+//         todo!()
+//     }
+// }
